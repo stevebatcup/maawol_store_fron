b3f2e5f8dcd1b891e83bbb708bc798f5d2383800
Steven Batcup <steve.batcup@gmail.com>
2020-10-15 17:58:26 +0100

    set correct env vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,13 +2,13 @@ fog_credentials = {
   provider:              'AWS',
   aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
   aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region:                'us-east-1',
+  region:                'us-east-1'
 }
 
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = fog_credentials
-  config.fog_directory  = 'maawol'
+  config.fog_directory = 'maawol'
 end
 
 Fog::Storage.new(fog_credentials).sync_clock
