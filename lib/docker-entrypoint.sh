9d91fd76d842739bba66232649135b94510c71dc
Steven Batcup <steve.batcup@gmail.com>
2020-10-21 13:46:59 +0100

    change to homepage pricing

--- lib/docker-entrypoint.sh
+++ lib/docker-entrypoint.sh
@@ -3,17 +3,10 @@
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
-bundle exec rails assets:precompile
 
 if [ $RAILS_ENV = 'production' ]
 then
-	echo "Copying assets to NGINX"
-	mkdir -p /usr/share/nginx/html/$APP_NAME
-	cp -R public/* /usr/share/nginx/html/$APP_NAME
-
-	echo "Copying NGINX config"
-	mkdir -p /etc/nginx/conf.d
-	cp config/nginx.conf /etc/nginx/conf.d/$APP_NAME.conf
+  bundle exec rails assets:precompile
 fi
 
 
